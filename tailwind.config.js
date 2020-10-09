c96d535d0061b8cb026d0a695c0f7738ca05d57e
Alex Duval <alexduval71@gmail.com>
2020-10-09 17:01:17 +0200

    add icon ready to share it

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,37 @@ module.exports = {
   theme: {
     minWidth: {
       '0': '0',
-      'popup': '350px',
+      'popup': '280px',
       full: '100%'
     },
     colors: {
-      'dsPurple': '#A700FF',
-      'dsYellow': '#FFEA00'
+      'dspurple': {
+        50: '#FBF2FF',
+        100: '#F6E6FF',
+        200: '#E9BFFF',
+        300: '#DC99FF',
+        400: '#C14DFF',
+        500: '#A700FF',
+        600: '#9600E6',
+        700: '#640099',
+        800: '#4B0073',
+        900: '#32004D',
+      },
+      'dsy1': '#FFD800',
+      'dsy2': '#FFEA00',
+      'dsy3': '#F5D905',
+      'dsyellow': {
+        50: '#FFFEF2',
+        100: '#FFFDE6',
+        200: '#FFFABF',
+        300: '#FFF799',
+        400: '#FFF04D',
+        500: '#FFEA00',
+        600: '#E6D300',
+        700: '#998C00',
+        800: '#736900',
+        900: '#4D4600',
+      },
     },
     extend: {
 
